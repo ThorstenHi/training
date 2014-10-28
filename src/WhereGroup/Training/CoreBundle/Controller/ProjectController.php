1ada6ca0acb6e20947f9587f0a7711e124ca3193
rpour <info@rpour.de>
2014-10-28 11:21:10 +0100

    .

--- src/WhereGroup/Training/CoreBundle/Controller/ProjectController.php
+++ src/WhereGroup/Training/CoreBundle/Controller/ProjectController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use WhereGroup\Training\CoreBundle\Entity\Project;
 
 /**
  * Class ProjectController
@@ -25,4 +26,63 @@ class ProjectController extends Controller
     {
         return array();
     }
+
+    /**
+     * @Route("/new", name="project_new")
+     * @Method("GET")
+     * @Template()
+     */
+    public function newAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/create", name="project_create")
+     * @Method("POST")
+     * @Template("TrainingCoreBundle:Project:new.html.twig")
+     */
+    public function createAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/edit/{id}", name="project_edit")
+     * @Method("GET")
+     * @Template("TrainingCoreBundle:Project:new.html.twig")
+     */
+    public function editAction($id)
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/update/{id}", name="project_update")
+     * @Method("POST")
+     * @Template("TrainingCoreBundle:Project:new.html.twig")
+     */
+    public function updateAction($id)
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/confirm/{id}", name="project_confirm_delete")
+     * @Method("GET")
+     * @Template()
+     */
+    public function confirmAction($id)
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/delete/{id}", name="project_delete")
+     * @Method("POST")
+     */
+    public function deleteAction($id)
+    {
+        return array();
+  }
 }
