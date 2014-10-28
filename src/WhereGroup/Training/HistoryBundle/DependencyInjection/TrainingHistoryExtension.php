5d95dbee8e1f3f74ead6d7f0c9eae9e0d2f8fda6
rpour <info@rpour.de>
2014-10-28 15:17:59 +0100

    History bundle

---
+++ src/WhereGroup/Training/HistoryBundle/DependencyInjection/TrainingHistoryExtension.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace WhereGroup\Training\HistoryBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * Class TrainingCoreExtension
+ *
+ * @package WhereGroup\Training\CoreBundle\DependencyInjection
+ * @author  A.R.Pour
+ */
+class TrainingHistoryExtension extends Extension
+{
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array            $config    An array of configuration values
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     *
+     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     *
+     * @api
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('parameters.yml');
+    }
+
+    /**
+     * Returns the recommended alias to use in XML.
+     *
+     * This alias is also the mandatory prefix to use when using YAML.
+     *
+     * This convention is to remove the "Extension" postfix from the class
+     * name and then lowercase and underscore the result. So:
+     *
+     *     AcmeHelloExtension
+     *
+     * becomes
+     *
+     *     acme_hello
+     *
+     * This can be overridden in a sub-class to specify the alias manually.
+     *
+     * @return string The alias
+     *
+     * @throws BadMethodCallException When the extension name does not follow conventions
+     */
+    public function getAlias()
+    {
+        return 'training_history';
+    }
+}
---
+++ src/WhereGroup/Training/HistoryBundle/TrainingHistoryBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace WhereGroup\Training\HistoryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class TrainingCoreBundle
+ *
+ * @package WhereGroup\Training\HistoryBundle
+ * @author  A.R.Pour
+ */
+class TrainingHistoryBundle extends Bundle
+{
+}
